a5d160439c2645954afa010d87def25e85c6f692
Ayush <mail@ayuch.dev>
2025-07-16 11:58:10 +0530

    fix: fix .gitignore, add `cmd/gitx`
    Signed-off-by: Ayush <mail@ayuch.dev>

---
+++ cmd/gitx/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gitxtui/gitx/internal/tui"
+)
+
+func main() {
+	app := tui.NewApp()
+	if err := app.Run(); err != nil {
+		log.Fatalf("Error running application: %v", err)
+	}
+}
