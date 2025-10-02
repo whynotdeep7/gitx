3d9482ce974bdc3de6e8f5464fec789387050e36
Divyansh Chawla <chawladivyansh131106@gmail.com>
2025-10-02 22:48:02 +0530

    feat: restrict gitx execution to git repositories (#23)
    Co-authored-by: Ayush Chauhan <56398566+bakayu@users.noreply.github.com>

--- cmd/gitx/main.go
+++ cmd/gitx/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"os/exec"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/gitxtui/gitx/internal/tui"
 	zone "github.com/lrstanley/bubblezone"
@@ -14,6 +14,11 @@ import (
 var version = "dev"
 
 func main() {
+	if err := ensureGitRepo(); err != nil {
+		fmt.Fprintln(os.Stderr, err) // print to stderr
+		os.Exit(1)
+  }
+  
 	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
 		fmt.Printf("gitx version: %s\n", version)
 		return
@@ -23,10 +28,19 @@ func main() {
 	defer zone.Close()
 
 	app := tui.NewApp()
+  
 	if err := app.Run(); err != nil {
 		if !errors.Is(err, tea.ErrProgramKilled) {
-			log.Fatalf("Error running application: %v", err)
+			log.Fatalf("error running application: %v", err)
 		}
 	}
 	fmt.Println("Bye from gitx! :)")
+}
+
+func ensureGitRepo() error {
+	cmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error: not a git repository")
+	}
+	return nil
 }
