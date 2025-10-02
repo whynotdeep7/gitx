055120b4c12c6cc19240f3eea8a87493ef726096
whynotdeep7 <deepakchoudhary3365@gmail.com>
2025-10-02 23:26:23 +0530

    Added the --help flag

--- cmd/gitx/main.go
+++ cmd/gitx/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/gitxtui/gitx/internal/tui"
 	zone "github.com/lrstanley/bubblezone"
@@ -13,22 +14,40 @@ import (
 
 var version = "dev"
 
+func printHelp() {
+	fmt.Println("gitx - A Git TUI Helper")
+	fmt.Println()
+	fmt.Println("Usage: gitx [options]")
+	fmt.Println()
+	fmt.Println("Options:")
+	fmt.Println("  -v, --version    Show version information")
+	fmt.Println("  -h, --help       Show this help message")
+	fmt.Println()
+	fmt.Println("Run 'gitx' inside a Git repository to start the TUI.")
+}
+
 func main() {
 	if err := ensureGitRepo(); err != nil {
 		fmt.Fprintln(os.Stderr, err) // print to stderr
 		os.Exit(1)
-  }
-  
-	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
-		fmt.Printf("gitx version: %s\n", version)
-		return
+	}
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "--version", "-v":
+			fmt.Printf("gitx version: %s\n", version)
+			return
+		case "--help", "-h":
+			printHelp()
+			return
+		}
 	}
 
 	zone.NewGlobal()
 	defer zone.Close()
 
 	app := tui.NewApp()
-  
+
 	if err := app.Run(); err != nil {
 		if !errors.Is(err, tea.ErrProgramKilled) {
 			log.Fatalf("error running application: %v", err)
